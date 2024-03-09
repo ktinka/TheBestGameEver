f365fb6e13253dfc7b55c35c0e7bce14e1da7bb5
ktinka <rshokira@gmail.com>
2024-03-09 11:20:00 +0300

    3 week
    я_хочу_спаать(

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed;
+    public float lifeTime;
+    public float damage = 10;
+
+     // Update is called once per frame
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += Time.fixedDeltaTime * speed * transform.forward;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+        DestroyFireball();
+    }
+
+    private void DamageEnemy(Collision collision)
+    {
+        var enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (enemyHealth != null)
+        {
+            enemyHealth.DealDamage(damage);
+        }
+    }
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+    private void Start()
+    {
+        Invoke("DestroyFireball", lifeTime);
+    }
+}
